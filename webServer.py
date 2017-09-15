e484453cfa87ec1fda3f55bd273fc2a738ab5d8a
Logan Girvin <logangirvin@hotmail.com>
2017-09-14 19:39:59 -0700

    MY BEAUTIFUL DUMB CHILD!!!~

--- webServer.py
+++ webServer.py
@@ -1,8 +1,8 @@
 import http.server
+import io
 import re
 import servoServer
 
-DEBUG = True
 API = re.compile('^/?api/?', re.IGNORECASE)
 SERVO = servoServer.Servo()
 
@@ -18,23 +18,29 @@ def main():
 class Dispatch(http.server.BaseHTTPRequestHandler):
     def do_GET(self):
         if (API.match(self.path)):
-            self.send_error(400, "API expects POST", "Api requests must be sent through POST, not GET")
-        self.send_response(204)
-        self.end_headers()
+            self.Api()
+        else:
+            super(Dispatch, self).do_GET()
 
     def do_POST(self):
-        if (API.match(self.path)):
-            self.Api()
-        self.send_response(204)
-        self.end_headers()
+        # although I originally intended for the API to use post, it appears python wants to fight me about reading post data.
+        self.send_error(405)
 
     def Api(self):
+        print("API")
         getPercent = re.compile('^/?api/getpercent', re.IGNORECASE)
-        setPercent = re.compile('^/?api/setpercent', re.IGNORECASE)
-
-def dprint(msg):
-    if (DEBUG):
-        print(msg)
+        setPercent = re.compile('^/?api/setpercent\?value=([0-9]+(\.[0-9]+)?)$', re.IGNORECASE)
+        val = setPercent.match(self.path)
+        if (val != None):
+            ival = float(val.group(1))
+            print(ival)
+            SERVO.set_percent(ival)
+            self.send_response(204)
+            self.end_headers()
+        elif (getPercent.match(self.path)):
+            print("GET!")
+        else:
+            self.send_error(400)
 
 if (__name__ == "__main__"):
     main()
