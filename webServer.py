a21f7f522b6fa5f0787b0936f2cf27120c63b699
Logan Girvin <logangirvin@hotmail.com>
2017-09-14 22:05:23 -0700

    Hey hey webserver and api are go.

--- webServer.py
+++ webServer.py
@@ -1,5 +1,6 @@
 import http.server
 import io
+import os
 import re
 import servoServer
 
@@ -9,6 +10,7 @@ SERVO = servoServer.Servo()
 def main():
     server_address = ('', 8000)
     httpd = http.server.HTTPServer(server_address, Dispatch)
+    SERVO.set_percent(0)
     try:
         httpd.serve_forever()
     except KeyboardInterrupt:
@@ -19,12 +21,25 @@ class Dispatch(http.server.BaseHTTPRequestHandler):
     def do_GET(self):
         if (API.match(self.path)):
             self.Api()
+            return
         else:
-            super(Dispatch, self).do_GET()
+            p = self.sanitized_path()
+            if (not os.path.isfile(p)):
+                self.send_error(404)
+                return
+            f = open(p, 'r')
+            out = bytes(f.read(), 'utf-8')
+            f.close()
+            self.send_response(200)
+            self.send_header('content-length', len(out))
+            self.end_headers()
+            self.wfile.write(out)
+            return
 
     def do_POST(self):
         # although I originally intended for the API to use post, it appears python wants to fight me about reading post data.
         self.send_error(405)
+        return
 
     def Api(self):
         print("API")
@@ -33,14 +48,27 @@ class Dispatch(http.server.BaseHTTPRequestHandler):
         val = setPercent.match(self.path)
         if (val != None):
             ival = float(val.group(1))
-            print(ival)
+            print("set: ", ival)
             SERVO.set_percent(ival)
             self.send_response(204)
             self.end_headers()
         elif (getPercent.match(self.path)):
-            print("GET!")
+            ival = SERVO.get_percent()
+            print("get: ", ival)
+            self.send_response(200)
+            out = bytes(str(ival), 'utf-8')
+            self.send_header('content-length', len(out))
+            self.end_headers()
+            self.wfile.write(out)
         else:
             self.send_error(400)
+    
+    def sanitized_path(self):
+        safe = re.sub(r'/\.\./', '/\\.\\./' , self.path)
+        safe = safe[1:]
+        if (safe is ''):
+            return "index.html"
+        return safe
 
 if (__name__ == "__main__"):
     main()
